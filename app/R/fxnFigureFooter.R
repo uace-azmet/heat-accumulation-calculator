1c087976df97e583f41eea2dc70f11a104bd3397
Jeremy Weiss <jlweiss@email.arizona.edu>
2024-07-26 16:00:51 -0700

    text updates to subtitle and footer

--- app/R/fxnFigureFooter.R
+++ app/R/fxnFigureFooter.R
@@ -76,13 +76,13 @@ fxnFigureFooter <- function(azmetStation, heatVariable, startDate, endDate, time
     htmltools::p(
       htmltools::HTML(
         paste0(
-          "Cumulative values are based on the sum of daily values of", " ", heatVariableText, " ", "for the current growing season (dark gray bar in graph) from", " ", gsub(" 0", " ", format(startDate, "%B %d, %Y")), " ", "through", " ", gsub(" 0", " ", format(endDate, "%B %d, %Y")), ".", " ", "Heat accumulation values for past seasons (gray bars in graph) are based on the same start and end month and day, but during those respective years", ".", " ", "More information on the calculation of heat units is in Extension bulletin", " ", bulletinURL, ".",
+          "Cumulative totals are based on the sum of daily totals of ", heatVariableText, " for the current growing season (dark gray bar in graph) from ", gsub(" 0", " ", format(startDate, "%B %d, %Y")), " through ", gsub(" 0", " ", format(endDate, "%B %d, %Y")), ". Heat accumulation totals for past seasons (gray bars in graph) are based on the same start and end month and day, but during those respective years. More information on the calculation of heat units is in Extension bulletin ", bulletinURL, ".",
           br(), br(), 
-          timeStep, " ", "AZMet data are from", " ", apiURL, " and accessed using the ", azmetrURL, " R package. Values from recent dates may be based on provisional data", ".", " ", "More information about", " ", webpageDataVariables, ",", " ", webpageNetworkMap, ",", " ", "and", " ", webpageStationMetadata, " ", "is available on the", " ", webpageAZMet, ".", " ", "Users of AZMet data and related information assume all risks of its use", ".",
+          timeStep, " AZMet data are from ", apiURL, " and accessed using the ", azmetrURL, " R package. Values from recent dates may be based on provisional data. More information about ", webpageDataVariables, ", ", webpageNetworkMap, ", and ", webpageStationMetadata, " is available on the ", webpageAZMet, ". Users of AZMet data and related information assume all risks of its use.",
           br(), br(),
           "To cite the above AZMet data, please use: 'Arizona Meteorological Network (", todayYear, ") Arizona Meteorological Network (AZMet) Data. https://azmet.arizona.edu. Accessed ", todayDate, "', along with 'Arizona Meteorological Network (", todayYear, ") Heat Accumulation Calculator. https://viz.datascience.arizona.edu/azmet/heat-accumulation-calculator. Accessed ", todayDate, "'.",
           br(), br(),
-          "For information on how this webpage is put together, please visit the", " ", webpageCode, " ", "for this tool."
+          "For information on how this webpage is put together, please visit the ", webpageCode, " for this tool."
         )
       )
     )
--- app/R/fxnFigureSubtitle.R
+++ app/R/fxnFigureSubtitle.R
@@ -4,10 +4,11 @@
 #' @param inData - data table of seasonal heat accumulation values by year
 #' @param startDate - Start date of period of interest
 #' @param endDate - End date of period of interest
+#' @param heatVariable - Heat variable selection by user
 #' @return `figureSubtitle` - Subtitle for figure based on selected AZMet station
 
 
-fxnFigureSubtitle <- function(azmetStation, inData, startDate, endDate) {
+fxnFigureSubtitle <- function(azmetStation, inData, startDate, endDate, heatVariable) {
   currentYear <- lubridate::year(endDate)
   previousYear <- currentYear - 1
   
@@ -19,12 +20,32 @@ fxnFigureSubtitle <- function(azmetStation, inData, startDate, endDate) {
   
   if (totalComparePreviousSum == 0) {
     compareTextPrevious <- "the same as"
-  } else if (totalComparePreviousSum > 0) {
+  } else if (totalComparePreviousSum > 0 & totalComparePreviousSum < 1) {
     compareTextPrevious <- 
       paste0(
         format(abs(round(totalComparePreviousSum, digits = 1)), nsmall = 1), " degree days Fahrenheit greater than"
       )
-  } else { # if (totalComparePreviousSum < 0)
+  } else if (totalComparePreviousSum == 1) {
+    compareTextPrevious <- 
+      paste0(
+        format(abs(round(totalComparePreviousSum, digits = 1)), nsmall = 1), " degree day Fahrenheit greater than"
+      )
+  } else if (totalComparePreviousSum > 1) {
+    compareTextPrevious <- 
+      paste0(
+        format(abs(round(totalComparePreviousSum, digits = 1)), nsmall = 1), " degree days Fahrenheit greater than"
+      )
+  } else if (totalComparePreviousSum < 0 & totalComparePreviousSum > -1) {
+    compareTextPrevious <- 
+      paste0(
+        format(abs(round(totalComparePreviousSum, digits = 1)), nsmall = 1), " degree days Fahrenheit less than"
+      )
+  } else if (totalComparePreviousSum == -1) {
+    compareTextPrevious <- 
+      paste0(
+        format(abs(round(totalComparePreviousSum, digits = 1)), nsmall = 1), " degree day Fahrenheit less than"
+      )
+  } else { # if (totalComparePreviousSum < -1)
     compareTextPrevious <- 
       paste0(
         format(abs(round(totalComparePreviousSum, digits = 1)), nsmall = 1), " degree days Fahrenheit less than"
@@ -38,7 +59,7 @@ fxnFigureSubtitle <- function(azmetStation, inData, startDate, endDate) {
       htmltools::p(
         htmltools::HTML(
           paste0(
-            "Heat accumulations at the AZMet ", azmetStation, " station from ", gsub(" 0", " ", format(startDate, "%B %d, %Y")), " through ", gsub(" 0", " ", format(endDate, "%B %d, %Y")), " is ", "<b>", format(round(currentYearHeatSum, digits = 1), nsmall = 1), " degree days Fahrenheit</b>."
+            "Heat accumulation at the AZMet ", azmetStation, " station from ", gsub(" 0", " ", format(startDate, "%B %d, %Y")), " through ", gsub(" 0", " ", format(endDate, "%B %d, %Y")), " is ", "<b>", format(round(currentYearHeatSum, digits = 1), nsmall = 1), " degree days Fahrenheit</b>."
           ),
         ),
         
